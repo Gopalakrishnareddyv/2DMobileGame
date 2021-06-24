f0806cb30502aba700a16de2991d294f36474b06
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-06-24 10:10:44 +0530

    Adde object pool method for enemy

--- Assets/EnemyRotation.cs
+++ Assets/EnemyRotation.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class EnemyRotation : MonoBehaviour
 {
     [SerializeField] float rotationspeed;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,5 +17,25 @@ public class EnemyRotation : MonoBehaviour
     {
         transform.Rotate(new Vector3(0, 0,rotationspeed*Time.deltaTime));
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "cat")
+        {
+            EnemySpawn.Instance.StopSpawn();
+        }
+        else
+        {
+            StartCoroutine(nameof(Bulletadd));
+        }
+    }
+    
+    IEnumerator Bulletadd()
+    {
+        yield return new WaitForSeconds(1);
+        if (rb.gameObject.name == "Enemy")
+        {
+            EnemySpawn.Instance.addEnemy(rb.gameObject);
+        }
     }
 }
--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject currentenemy;
+    Stack<GameObject> enemypool = new Stack<GameObject>();
+    private static EnemySpawn instance;
+
+    public static EnemySpawn Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<EnemySpawn>();
+            }
+            return instance;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         StartSpawn();
     }
-
+    public void CreatePool()
+    {
+        enemypool.Push(Instantiate(enemy));
+        enemypool.Peek().SetActive(false);
+        enemypool.Peek().name = "Enemy";
+    }
+    public void addEnemy(GameObject enemytemp)
+    {
+        enemypool.Push(enemytemp);
+        enemypool.Peek().SetActive(false);
+    }
     // Update is called once per frame
     public void SpawnEnemy()
     {
+        if (enemypool.Count == 0)
+        {
+            CreatePool();
+        }
+        GameObject temp = enemypool.Pop();
+        temp.SetActive(true);
         float xvalue = Random.Range(-2.0f, 2.0f);
         Vector2 spawnposition = new Vector2(xvalue, 6.0f);
-        Instantiate(enemy,spawnposition, Quaternion.identity);
+        temp.transform.position = spawnposition;
+        currentenemy = temp;
 
     }
     public void StartSpawn()
     {
-        InvokeRepeating("SpawnEnemy", 1.0f, 2.0f);
+        InvokeRepeating("SpawnEnemy", 1.0f, 4.0f);
     }
     public void StopSpawn()
     {
